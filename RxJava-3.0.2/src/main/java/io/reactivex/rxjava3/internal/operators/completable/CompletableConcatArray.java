1a3948f033f5f620f9bb3ad10b3b25d20efd61b8
ritambharajha <rj6463@g.rit.edu>
2022-03-28 12:37:00 -0400

    ResolvedDefect9

--- RxJava-3.0.2/src/main/java/io/reactivex/rxjava3/internal/operators/completable/CompletableConcatArray.java
+++ RxJava-3.0.2/src/main/java/io/reactivex/rxjava3/internal/operators/completable/CompletableConcatArray.java
@@ -21,11 +21,11 @@ import io.reactivex.rxjava3.internal.disposables.SequentialDisposable;
 
 public final class CompletableConcatArray extends Completable {
     final CompletableSource[] sources;
-    //CompletableSource[] sourcesa;
-// FIXME: Need to fix the exposing of the internal variable to the external class
+    CompletableSource[] sourcesa;
     public CompletableConcatArray(CompletableSource[] sources) {
-        //sourcesa = sources;
         this.sources = sources;
+        sourcesa = sources;
+
     }
 
     @Override
