7b092bbe41ec9d537d4784180dfb61f96d89c2bb
ritambharajha <rj6463@g.rit.edu>
2022-03-28 12:31:52 -0400

    ResolvedDefect7

--- RxJava-3.0.2/src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableWindowBoundarySelector.java
+++ RxJava-3.0.2/src/main/java/io/reactivex/rxjava3/internal/operators/observable/ObservableWindowBoundarySelector.java
@@ -72,8 +72,8 @@ public final class ObservableWindowBoundarySelector<T, B, V> extends AbstractObs
         final AtomicBoolean downstreamDisposed;
 
         final AtomicLong requested;
-        //FIXME: remove unused fields
-        long emitted;
+
+        //long emitted;
 
         volatile boolean upstreamCanceled;
 
